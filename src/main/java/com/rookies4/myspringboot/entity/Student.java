460b9c6933d15baade9a25476239a384e07d01b5
kms55937256 <kms55937256@gmail.com>
2025-08-20 17:16:51 +0900

    student.dto

--- src/main/java/com/rookies4/myspringboot/entity/Student.java
+++ src/main/java/com/rookies4/myspringboot/entity/Student.java
@@ -14,16 +14,27 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Student {
-    
+    //PK (Sequential Value)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
     private Long id;
-    
+
+    //이름
     @Column(nullable = false)
     private String name;
-    
-    @Column(nullable = false, unique = true)
+
+    //학번
+    @Column(unique = true, nullable = false)
     private String studentNumber;
-    
+
+    /*
+        양방향관계 Student에서 StudentDetail을 참조할 수 있도록
+        FK에 해당하는 필드명을 mappedBy에 설정한다.*
+     */
+    //1:1관계 지연로딩
+    @OneToOne(fetch = FetchType.LAZY,
+            mappedBy = "student",
+            cascade = CascadeType.ALL)
     private StudentDetail studentDetail;
 }
